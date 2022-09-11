819038d50972e0ab166e4932976215eb0b723a91
tsunyoku <mbruhyo@gmail.com>
2022-09-11 11:04:59 +0100

    fix: move collection fetching into try block, return on exception

--- OsuCollectorDownloaderCLI/Program.cs
+++ OsuCollectorDownloaderCLI/Program.cs
@@ -13,16 +13,17 @@ var collectionIds = collectionsString.Split(",", StringSplitOptions.RemoveEmptyE
 Console.Write("How would you like to save the collections?\n\n1) Save beatmaps to zip\n2) Save to .osdb file\n\nYour selection: ");
 var saveOption = (SaveOption)int.Parse(Console.ReadLine()!);
 
-var collections = await OsuCollector.FetchCollections(collectionIds);
-
 try
 {
+    var collections = await OsuCollector.FetchCollections(collectionIds);
     await CollectionProcessor.Process(collections, saveOption);
 }
 catch (Exception e)
 {
     Console.WriteLine(e);
     Console.ReadKey();
+
+    return;
 }
 
 Console.WriteLine("Finished fetching collections.");
