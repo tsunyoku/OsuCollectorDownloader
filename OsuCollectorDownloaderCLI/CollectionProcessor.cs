9196141e1bf15ed83e1882de2715862b413711c9
tsunyoku <mbruhyo@gmail.com>
2022-09-11 11:03:41 +0100

    refactor: process collection downloads in a separate place so exceptions can be handled more gracefully

---
+++ OsuCollectorDownloaderCLI/CollectionProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Immutable;
+using OsuCollectorDownloader.Helpers;
+using OsuCollectorDownloaderCLI.Enums;
+using OsuCollectorDownloaderLib;
+using OsuCollectorDownloaderLib.Helpers;
+using OsuCollectorDownloaderLib.Models;
+
+namespace OsuCollectorDownloaderCLI;
+
+public static class CollectionProcessor
+{
+    public static async Task Process(ImmutableArray<Collection> collections, SaveOption saveOption)
+    {
+        var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "collections");
+        if (!Directory.Exists(folderPath))
+            Directory.CreateDirectory(folderPath);
+
+        foreach (var collection in collections)
+        {
+            Console.WriteLine($"Fetched collection \"{collection.CollectionDetails.Name}\" (ID {collection.CollectionDetails.Id}).");
+
+            var beatmapsetIds = collection.BeatmapDetails.Beatmaps.Select(x => x.BeatmapsetId).ToImmutableArray();
+
+            var filePath = string.Empty;
+            IWriter? writer = null;
+
+            switch (saveOption)
+            {
+                case SaveOption.ZipFile:
+                    filePath = Path.Combine(folderPath, $"{collection.CollectionDetails.Name}.zip");
+                    var beatmapsetDownloads =
+                        await BeatmapsetDownloader.DownloadBeatmapsets(beatmapsetIds);
+
+                    writer = new ZipWriter(beatmapsetDownloads, filePath);
+                    break;
+                case SaveOption.OsdbFile:
+                    filePath = Path.Combine(folderPath, $"{collection.CollectionDetails.Name}.osdb");
+                    writer = new OsdbWriter(collection, filePath);
+                    break;
+            }
+
+            if (writer is null)
+                throw new Exception($"Invalid download option {saveOption}");
+
+            writer.Write();
+            Console.WriteLine($"Saved collection \"{collection.CollectionDetails.Name}\" to {filePath}");
+        }
+    }
+}
--- OsuCollectorDownloaderCLI/Program.cs
+++ OsuCollectorDownloaderCLI/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Immutable;
-using OsuCollectorDownloader.Helpers;
+using OsuCollectorDownloaderCLI;
 using OsuCollectorDownloaderCLI.Enums;
 using OsuCollectorDownloaderLib;
-using OsuCollectorDownloaderLib.Helpers;
-using OsuCollectorDownloaderLib.Models;
 
 Console.WriteLine("OsuCollector Downloader tool");
 Console.Write("Enter collection IDs you wish to download, separated by a comma (,): ");
@@ -13,43 +11,18 @@ var collectionIds = collectionsString.Split(",", StringSplitOptions.RemoveEmptyE
     .Select(int.Parse).ToImmutableArray();
 
 Console.Write("How would you like to save the collections?\n\n1) Save beatmaps to zip\n2) Save to .osdb file\n\nYour selection: ");
-var downloadOption = (SaveOption)int.Parse(Console.ReadLine()!);
+var saveOption = (SaveOption)int.Parse(Console.ReadLine()!);
 
 var collections = await OsuCollector.FetchCollections(collectionIds);
 
-var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "collections");
-if (!Directory.Exists(folderPath))
-    Directory.CreateDirectory(folderPath);
-
-foreach (var collection in collections)
+try
 {
-    Console.WriteLine($"Fetched collection \"{collection.CollectionDetails.Name}\" (ID {collection.CollectionDetails.Id}).");
-
-    var beatmapsetIds = collection.BeatmapDetails.Beatmaps.Select(x => x.BeatmapsetId).ToImmutableArray();
-
-    var filePath = string.Empty;
-    IWriter? writer = null;
-
-    switch (downloadOption)
-    {
-        case SaveOption.ZipFile:
-            filePath = Path.Combine(folderPath, $"{collection.CollectionDetails.Name}.zip");
-            var beatmapsetDownloads =
-                await BeatmapsetDownloader.DownloadBeatmapsets(beatmapsetIds);
-
-            writer = new ZipWriter(beatmapsetDownloads, filePath);
-            break;
-        case SaveOption.OsdbFile:
-            filePath = Path.Combine(folderPath, $"{collection.CollectionDetails.Name}.osdb");
-            writer = new OsdbWriter(collection, filePath);
-            break;
-    }
-
-    if (writer is null)
-        throw new Exception($"Invalid download option {downloadOption}");
-
-    writer.Write();
-    Console.WriteLine($"Saved collection \"{collection.CollectionDetails.Name}\" to {filePath}");
+    await CollectionProcessor.Process(collections, saveOption);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    Console.ReadKey();
 }
 
 Console.WriteLine("Finished fetching collections.");
